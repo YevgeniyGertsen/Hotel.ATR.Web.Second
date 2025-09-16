c8cbf983e24ae0f97e7d880b36a13b3dbd0c9a27
YevgeniyGertsen <gersen.e.a@gmail.com>
2025-09-16 11:27:51 +0500

    Добавил представление

---
+++ Hotel.ATR.Web.Second/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Web.Second.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
